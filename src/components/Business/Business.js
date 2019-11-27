3bf780326b62da125b77f126afc1f42830d70a09
Mousa Ayoubi <mousaayoubi@gmail.com>
2019-11-27 16:51:13 +0200

    Update files

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -21,15 +21,15 @@ class Business extends React.Component {
                     <img src={business.imageSrc} alt=''/>
                 </div>
                     <h2>{business.name}</h2>
-                <div class="Business-information">
-                    <div class="Business-address">
+                <div className="Business-information">
+                    <div className="Business-address">
                     <p>{business.address}</p>
                     <p>{business.city}</p>
                     <p>{business.state} {business.zipCode}</p>
                     </div>
-                    <div class="Business-reviews">
+                    <div className="Business-reviews">
                     <h3>{business.category}</h3>
-                    <h3 class="rating">{business.rating} stars</h3>
+                    <h3 className="rating">{business.rating} stars</h3>
                     <p>{business.reviewCount} reviews</p>
                     </div>
                 </div>
