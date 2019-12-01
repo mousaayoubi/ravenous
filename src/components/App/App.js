29368aebf2b5c16203085ff796e99d7a49184774
Mousa Ayoubi <mousaayoubi@gmail.com>
2019-12-01 18:02:33 +0200

    Update files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,7 @@ import SearchBar from '../SearchBar/SearchBar';
 
 const business = {
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'Luca Pizzeria',
+  name: 'MarginOtto Pizzeria',
   address: '1010 Paddington Way',
   city: 'Flavortown',
   state: 'NY',
