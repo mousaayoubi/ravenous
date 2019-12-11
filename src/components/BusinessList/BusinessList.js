ebfe38d43d1baeec19664f4fd0dd44d00aaffac2
Mousa Ayoubi <mousaayoubi@gmail.com>
2019-12-11 16:04:38 +0200

    Update files

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -5,7 +5,7 @@ import Business from '../Business/Business';
 class BusinessList extends React.Component {
     render() {
         return (
-            <div className="BusinessList" key='businessListKey'>
+            <div className="BusinessList" >
                 {this.props.businesses.map(business => {
                     return <Business business={business} key={business.id}/>
                 })}
